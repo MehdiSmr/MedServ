297a79cc632ef2044a6bf1e1cc2fae5b3e2afbdf
Mehdi <semmarmehdi558@gmail.com>
2025-08-29 00:23:52 +0100

    implemented client requests

--- client.cpp
+++ client.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <vector>
 #include <cstring>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
 #define PORT 8989
+
+int getChatRequest(const char* chat, int client_fd)
+{
+    std::string request = "GET /chat/" + std::string(chat); 
+    const int status = send(client_fd, request.c_str(), strlen(request.c_str()), 0);
+    if (status < 0) {
+        std::cerr << "Sending Request error" << std::endl;
+        return -1;
+    }
+    return 0;
+};
+
+int getChatsRequest(const char* username, int client_fd)
+{
+    const char* request = "GET /chat"; 
+    const int status = send(client_fd, request, strlen(request), 0);
+    if (status < 0) {
+        std::cerr << "Sending Request error" << std::endl;
+        return -1;
+    }
+    return 0;
+};
+
+int createChatRequest(const char* chat, const char* username, int client_fd)
+{
+    std::string request = "POST /chat/" + std::string(chat); 
+    const int status = send(client_fd, request.c_str(), strlen(request.c_str()), 0);
+    if (status < 0) {
+        std::cerr << "Sending Request error" << std::endl;
+        return -1;
+    }
+    return 0;
+};
+
+int sendMessageRequest(const char* message, const char* username, int client_fd)
+{
+    std::string request = "GET /message/" + std::string(message); 
+    const int status = send(client_fd, request.c_str(), strlen(request.c_str()), 0);
+    if (status < 0) {
+        std::cerr << "Sending Request error" << std::endl;
+        return -1;
+    }
+    return 0;
+};
+
 
 int main() {
     int status, client_fd;
@@ -36,30 +82,38 @@ int main() {
         return -1;
     }
 
-    char* username = new char[1024];
+    char username[1024];
+    std::vector<std::string> chats;
+    bool session{true};
+    bool inChat{false};
+    
     std::cout << "Enter username: ";
     std::cin >> username;
-    
-    status = send(client_fd, username, strlen(username), 0);
-    if (status < 0) {
-        perror("send error");
-        std::cout << "errno: " << errno << std::endl;
-    }
-    
-    char buffer[1024];
-    memset(buffer, 0, sizeof(buffer));
 
-    ssize_t bytes_read = recv(client_fd, buffer, 1023, 0);
-    
-    if (bytes_read > 0) {
-        buffer[bytes_read] = '\0';  // Null terminate
-        std::string response(buffer);
-        std::cout << "Received response: '" << response << "'" << std::endl;
-    } else if (bytes_read == 0) {
-        std::cout << "Connection closed by server" << std::endl;
-    } else {
-        perror("recv error");
-        std::cout << "errno: " << errno << std::endl;
+    while (session) { 
+        char buffer[1024];
+        memset(buffer, 0, sizeof(buffer));
+
+        if (!inChat) {
+            getChatsRequest(username, client_fd); // est ce qu'on attend la prochaine iteration ou est ce j'enchaine tout
+        } else {
+        }
+
+
+        ssize_t bytes_read = recv(client_fd, buffer, 1023, 0);
+        
+        if (bytes_read > 0) {
+            buffer[bytes_read] = '\0';  // Null terminate
+            std::string response(buffer);
+            std::cout << "Received response: '" << response << "'" << std::endl;
+        } else if (bytes_read == 0) {
+            std::cout << "Connection closed by server" << std::endl;
+            session = false;
+        } else {
+            perror("recv error");
+            std::cout << "errno: " << errno << std::endl;
+            session = false;
+        }
     }
     
     close(client_fd);
--- makefile
+++ makefile
@@ -4,6 +4,6 @@ clean:
 	rm -f client server user/user
 
 classes:
-	clang++ -Wall -o client client.cpp
-	clang++ -Wall -o server server.cpp
+	clang++ -Werror -std=c++20 -o client client.cpp
+	clang++ -Werror -std=c++20 -o server server.cpp
 
